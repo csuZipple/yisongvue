e93e17893c5c293dfb5bf0057c476ae7d4f33397
csuZipple <csu_zipple@qq.com>
2018-12-14 19:40:01 +0800

    Revert "fix: the bottom navigation did not selected with the route changed."
    This reverts commit b30f936

--- src/components/container/FooterWrapper.vue
+++ src/components/container/FooterWrapper.vue
@@ -1,21 +1,21 @@
 <template>
-  <tabbar>
-    <tabbar-item :selected="selected===0"  link="home">
+  <tabbar @on-index-change="change">
+    <tabbar-item selected  link="home">
       <img slot="icon" src="../../assets/home.png">
       <span slot="label">首页</span>
     </tabbar-item>
 
-    <tabbar-item :selected="selected===1" show-dot link="cart">
+    <tabbar-item show-dot link="cart">
       <img slot="icon" src="../../assets/home.png">
       <span slot="label">购物车</span>
     </tabbar-item>
 
-    <tabbar-item :selected="selected===2" link="order">
+    <tabbar-item  link="order">
       <img slot="icon" src="../../assets/home.png">
       <span slot="label">订单</span>
     </tabbar-item>
 
-    <tabbar-item :selected="selected===3" link="user" badge="3">
+    <tabbar-item  link="user" badge="3">
       <img slot="icon" src="../../assets/home.png">
       <img slot="icon-active" src="../../assets/logo.png">
       <span slot="label">我的</span>
@@ -29,27 +29,12 @@
   export default {
     name: "Footer",
     components: {Tabbar,TabbarItem},
-    computed:{
-      selected(){
-          const path = this.$route.path;
-          let index = 0;
-          switch(path){
-            case "/yisong/home":
-              break;
-            case "/yisong/cart":
-              index = 1;
-              break;
-            case "/yisong/order":
-              index = 2;
-              break;
-            case "/yisong/user":
-              index = 3;
-              break;
-            default:
-          }
-          return index;
+    methods:{
+      change(v){
+        console.log(v)
       }
     }
+
   }
 </script>
 
