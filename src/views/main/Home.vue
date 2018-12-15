65267b3210a3de688a48e03fd5bc6f18f92c249c
csuZipple <csu_zipple@qq.com>
2018-12-15 10:59:52 +0800

    feat: add search component in home(.vue)

--- src/views/main/Home.vue
+++ src/views/main/Home.vue
@@ -1,15 +1,28 @@
 <template>
-    <p>
-      This is home page
-    </p>
+    <div>
+      <div class="ys-banner">
+        <div class="wrapper">
+          <Search link="/search" placeholder="干脆面"/>
+        </div>
+
+      </div>
+    </div>
 </template>
 
 <script>
+    import Search from "./components/Search";
     export default {
-        name: "Home"
+        name: "Home",
+        components: {Search}
     }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+  .ys-banner{
+    .wrapper{
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  }
 </style>
--- src/views/main/components/Search.vue
+++ src/views/main/components/Search.vue
@@ -1,12 +1,46 @@
 <template>
+  <div v-text="placeholder" class="ys-search" @click="onSearchClicked"></div>
 </template>
 
 <script>
-    export default {
-        name: "Search"
+  export default {
+    name: "Search",
+    props:{
+      placeholder:String,
+      link:String
+    },
+    methods:{
+      onSearchClicked(){
+        console.log(this.link);
+        this.$router.push(this.link);
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  @height:50px;
+  .ys-search{
+    width: 80%;
+    height: @height;
+    border-radius:25px;
+    background: rgba(255,255,255,.9);
+    position: relative;
+    color: rgb(199,199,199);
+    line-height: @height;
+    padding-left: 94px;
+    border: 1px solid #eee;
 
+    &:after{
+      content: '';
+      width: 22px;
+      height: 22px;
+      background: url("../../../assets/icon/search.svg") center no-repeat;
+      background-size:contain;
+      position: absolute;
+      left: 40px;
+      top: 50%;
+      transform:translateY(-50%);
+    }
+  }
 </style>
