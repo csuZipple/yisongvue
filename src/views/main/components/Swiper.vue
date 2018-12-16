b64dfc068bbc6ccbb62e42c75e471f8560465c0c
csuZipple <csu_zipple@qq.com>
2018-12-16 15:25:16 +0800

    refactor: support animation and dot

--- src/views/main/components/Swiper.vue
+++ src/views/main/components/Swiper.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="ys-swiper">
-    <ul>
-      <li v-for="item in list">
+    <transition-group tag="ul" class="swiper-item" enter-active-class="animated bounceIn" leave-active-class="animated fadeOut">
+      <li v-for="(item,index) in list" v-show="index === currentIndex" :key="item.id">
         <img :src="item.src" :alt="item.alt">
       </li>
-    </ul>
+    </transition-group>
     <ul v-if="showDot" class="dot-list">
-      <li v-for="dot in list.length" class="dot"></li>
+      <li v-for="(dot,index) in list.length" class="dot" :class="{'dot-active':index===currentIndex}"></li>
     </ul>
   </div>
 </template>
 
 <script>
+  import animated from 'animate.css'
   export default {
     name: "Swiper",
+    data(){
+      return{
+        timer:null,
+        currentIndex:0
+      }
+    },
     props:{
       list:{
         type:Array,
@@ -22,34 +29,28 @@
         }
       }
     },
-    created(){
-      console.log("created SwipeList");
-      console.log(this.list)
-    },
-    mounted(){
-      console.log("mounted SwipeList");
-      console.log(this.list)
-    },
-    beforeUpdate(){
-      console.log("beforeUpdate SwipeList");
-      console.log(this.list)
-    },
-    updated(){
-      console.log("updated SwipeList");
-      console.log(this.list)
-    },
     computed:{
       showDot(){
         return this.list.length>1;
       }
     },
+    beforeDestroy(){
+      this.timer && clearInterval(this.timer);
+    },
+
+    methods:{
+      animation(){
+        console.log("Switch!@");
+        // this.list.push(this.list.shift()); -- error: will call the watcher!
+        this.currentIndex = (this.currentIndex+1)%4;
+      }
+    },
     watch:{
-      list:{
-        handler(n,o){
-          console.log('list-n',n);
-          console.log('list-o',n);
-        },
-        deep:true
+      list:function (n,o) {
+        console.log(n);
+        if(n.length>1){
+          this.timer = setInterval(this.animation,3000);
+        }
       }
     }
   }
@@ -62,12 +63,8 @@
     width: 100%;
     height: 100%;
     position: relative;
+    overflow: hidden;
     ul{
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      left: 0;
-      top: 0;
       li{
         list-style: none;
         width: 100%;
@@ -78,7 +75,14 @@
           object-fit: cover;
         }
       }
-
+    }
+    .swiper-item{
+      width: 100%;
+      height: 100%;
+      position: relative;
+      li{
+        position: absolute;
+      }
     }
 
     .dot-list{
@@ -92,8 +96,12 @@
         vertical-align: middle;
         width: 6px;
         height: 6px;
+        margin: 3px;
         border-radius: 3px;
         background-color: @dot-color;
+      }
+      .dot-active{
+        background-color: @dot-color-active;
       }
     }
   }
