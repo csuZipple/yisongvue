9d7194dbff5c650f30be0f9741fcb92a4bee3055
csuZipple <csu_zipple@qq.com>
2018-12-18 21:39:32 +0800

    feat: mock order data and complete OrderItem.vue

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
       <Tab class="tab"/>
+      <div class="ys-order">
+        <OrderItem v-for="(item,index) in orderList" v-bind="item" v-bind:key="index"/>
+      </div>
   </div>
 </template>
 
 <script>
   import Tab from "./components/Tab";
+  import OrderItem from "./components/OrderItem";
+  import { createNamespacedHelpers } from 'vuex'
+  const { mapState, mapActions } = createNamespacedHelpers('data');
   export default {
     name: "Order",
-    components: {Tab}
+    components: {OrderItem, Tab},
+    computed:{
+      ...mapState(['orderList'])
+    }
   }
 </script>
 
 <style lang="less" scoped>
 .tab{
-  padding: 20px 0 20px 30px;
+  padding: 30px 0 30px 30px;
 }
+  .ys-order{
+    padding: 3px 5vw;
+  }
 </style>
---
+++ src/views/order/components/OrderItem.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="ys-order-item">
+    <p class="title">
+      <span style="font-weight: 500;">{{shopName}}</span>
+      <span style="color: #FFB93D;font-size:3vw;">{{state[status]}}</span>
+    </p>
+    <div class="product-wrapper">
+      <div class="product">
+        <figure class="item" v-for="(item,index) in products" v-bind:key="index">
+          <img :src="item.image" :alt="item.alt">
+          <figcaption>
+            {{item.title}}
+          </figcaption>
+        </figure>
+      </div>
+      <a class="link" href="javascript:;" @click="">
+
+      </a>
+    </div>
+    <p class="desc">订单号：{{orderId}}</p>
+    <p class="desc">送餐时间：{{time}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "OrderItem",
+    props:{
+      shopName:String,
+      status:Number,
+      state:{
+        type:Array,
+        default(){
+          return ['订单交易中','交易完成','未付款'];
+        }
+      },
+      products:Array,
+      orderId:[String,Number],
+      time:String
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @width:2vw;
+  @beforeWidth:1vw;
+  .ys-order-item{
+    border-radius:8px;
+    box-shadow:0 0 30px 3px rgba(0,0,0,0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    p{
+      padding: 1vw @width;
+      color: #262626;
+    }
+
+    .product-wrapper{
+
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      margin: 0 2vw;
+      padding: 10px 0;
+      background: rgba(245,245,245,1);
+
+      .link{
+        width: 30px;
+        height: 30px;
+        background: url("../../../assets/icon/order-link.svg") no-repeat center;
+        background-size:50%;
+        margin-top: -20px;
+      }
+    }
+    .product{
+      display: flex;
+      justify-content: space-around;
+
+      .item{
+        padding-right: 20px;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        img{
+          width: 12vw;
+          object-fit: contain;
+          margin-bottom:6px;
+        }
+
+        figcaption{
+          margin-top:  6px;
+        }
+
+        &:after{
+          content: '+';
+          position: absolute;
+          font-size: 24px;
+          width: 30px;
+          height: 30px;
+          text-align: center;
+          line-height: 30px;
+          color: #AEA9AA;
+          right: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          margin-top: -10px;
+        }
+        &:last-child{
+          padding-right: 0;
+          &:after{
+            content: '';
+          }
+        }
+      }
+
+    }
+    .title{
+      height: 8vw;
+      font-size:4vw;
+      display:flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #333333;
+      position: relative;
+      &:before{
+        content: '';
+        background: #FFB93D;
+        width: @beforeWidth;
+        height: 70%;
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+
+    .desc{
+      padding-left: 3vw;
+    }
+  }
+</style>
