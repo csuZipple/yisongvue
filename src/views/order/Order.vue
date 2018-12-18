6859acc1cd6cc9d38d4a32e48b79d3926ab2632f
csuZipple <csu_zipple@qq.com>
2018-12-18 19:15:48 +0800

    feat: add tab in order.vue

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,15 +1,19 @@
 <template>
-    <p>
-      This is order!
-    </p>
+  <div>
+      <Tab class="tab"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Order"
-    }
+  import Tab from "./components/Tab";
+  export default {
+    name: "Order",
+    components: {Tab}
+  }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.tab{
+  padding: 20px 0 20px 30px;
+}
 </style>
---
+++ src/views/order/components/Tab.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ys-tab">
+    <a :class="['ys-tab-item',{'active':currentIndex===index}]" href="javascript:;" v-for="(item,index) in text" @click="changeTab(index)">{{item}}</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Tab",
+    methods:{
+      changeTab(index){
+        this.currentIndex = index;
+        this.$emit('changeTab',index);
+      }
+    },
+    data(){
+      return{
+        text:["待取","全部"],
+        currentIndex:0
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .ys-tab{
+    display: flex;
+    .ys-tab-item{
+      border: 1px solid #FFE0AD;
+      color: #666666;
+      font-size:4.5vw;
+      padding: 5px 35px;
+      border-radius:20px;
+
+      &:not(:first-child){
+        margin-left: -30px;
+      }
+    }
+    .active{
+      background: #FFDF5C;
+      color: #333333;
+    }
+  }
+</style>
