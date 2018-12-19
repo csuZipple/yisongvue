e15d3f09a2b8112c9fbc5ccf57c01f8ec9878e2a
csuZipple <csu_zipple@qq.com>
2018-12-19 19:44:57 +0800

    feat: complete search component

--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -1,13 +1,90 @@
 <template>
-  <p>I am search page</p>
+  <div>
+    <YsHeader style="border-bottom:none;height: 80px;" :show-right-text="true" :right-text="rightText" @onRightClicked="onSearch">
+       <div class="ys-search-input-wrapper">
+         <img src="../../assets/icon/search.svg" alt="search">
+         <input v-model="keyword" placeholder="小浣熊"/>
+       </div>
+    </YsHeader>
+    <SearchTips title="热门搜索" v-bind:data="mockData"/>
+    <transition leave-active-class="animated fadeOutLeft">
+      <SearchTips v-if="showHistory" title="历史记录" :show-delete="true" @delete="showHistory = !showHistory"/>
+    </transition>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Search"
+  import animated from 'animate.css'
+  import SearchTips from "./components/SearchTips";
+  import YsHeader from "../../components/Header";
+  export default {
+    name: "Search",
+    components: {YsHeader, SearchTips},
+    methods:{
+      onSearch(){
+        console.log("search---",this.keyword);
+        this.keyword = '';
+      }
+    },
+    data(){
+      return{
+        showHistory:true,
+        rightText:"搜索",
+        keyword:"",
+        mockData:[
+          {
+            productId:1,
+            name:'和成天下'
+          },
+          {
+            productId:2,
+            name:'卫龙'
+          },
+          {
+            productId:3,
+            name:'雪花'
+          },
+          {
+            productId:4,
+            name:'统一老坛酸菜'
+          },
+          {
+            productId:5,
+            name:'芙蓉王'
+          },
+          {
+            productId:6,
+            name:'汤达人'
+          }
+        ]
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .ys-search-input-wrapper {
+    width: 68%;
+    height: 50%;
+    background: rgba(0, 0, 0, .06);
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
 
+    img{
+      width: 22px;
+      height: 22px;
+      padding: 3px;
+      margin: 0 8px;
+      object-fit: contain;
+    }
+
+    input{
+      background: transparent;
+    }
+    input:focus{
+      border: none;
+      outline: none;
+    }
+  }
 </style>
---
+++ src/views/Search/components/SearchTips.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="ys-search-tips">
+    <p>
+      <span>{{title}}</span>
+      <img v-if="showDelete" src="../../../assets/icon/delete.svg" @click="handleDeleteClicked" alt="删除">
+    </p>
+    <div class="wrapper">
+      <a class="item" href="javascript:;" @click="search(item.productId)" v-for="(item,index) of data" v-bind:key="index">
+        {{item.name}}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SearchTips",
+    props:{
+      showDelete:{
+        type:Boolean,
+        default:false
+      },
+      title:String,
+      data:{
+        type:Array,
+        default(){
+          return[
+            {
+              productId:1,
+              name:'统一老坛酸菜牛肉面'
+            }
+          ]
+        }
+      }
+    },
+    methods:{
+      handleDeleteClicked(){
+        this.$emit("delete",!this.showDelete)
+      },
+      search(productId){
+        console.log("search id: ",productId)
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .ys-search-tips{
+    padding: 8px 12px;
+
+    p{
+      padding: 8px;
+      color: #999999;
+      font-size: 4vw;
+
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      img{
+        width: 5vw;
+        height: 5vw;
+        object-fit: contain;
+      }
+    }
+
+    .wrapper{
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+
+      .item{
+        padding: 5px 8px;
+        margin: 5px 8px;
+        background: rgba(0,0,0,.06);
+        border-radius:3px;
+        color: #333333;
+        font-size: 4vw;
+      }
+    }
+  }
+</style>
