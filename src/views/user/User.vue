a9a22df6d9f8e61eb92b572919ace3fabba74ab5
csuZipple <csu_zipple@qq.com>
2018-12-19 17:36:23 +0800

    feat: complete the user card component

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,22 +1,59 @@
 <template>
   <div>
     <Avatar v-bind:username="user.username" v-bind:phone="user.phone"/>
+    <div class="ys-card">
+      <Card v-for="(item,index) in cardList" v-bind:key="index" v-bind="item"/>
+    </div>
   </div>
 </template>
 
 <script>
   import Avatar from "./components/Avatar";
   import {createNamespacedHelpers} from 'vuex'
+  import Card from "./components/Card";
   const {mapState,mapActions} = createNamespacedHelpers("data");
   export default {
     name: "User",
-    components: {Avatar},
+    components: {Card, Avatar},
     computed:{
       ...mapState(['user'])
+    },
+    data(){
+      return{
+        cardList:[
+          {
+            icon:require("../../assets/icon/member.svg"),
+            title:"会员充值",
+            desc:"余额：￥16.8<br/>积分：2087"
+          },
+          {
+            icon:require("../../assets/icon/setting.svg"),
+            title:"修改密码",
+            desc:"密码修改，保护资金安全"
+          },
+          {
+            icon:require("../../assets/icon/address.svg"),
+            title:"地址管理",
+            desc:"中南大学铁道学院 通泰梅岭苑"
+          },
+          {
+            icon:require("../../assets/icon/notice.svg"),
+            title:"帮助中心",
+            desc:"您有三条消息未读"
+          }
+        ]
+      }
     }
   }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .ys-card{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 3vw;
+    grid-row-gap: 3vw;
 
+    padding: 20px;
+  }
 </style>
---
+++ src/views/user/components/Card.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ys-cart-item">
+    <img :src="icon" alt="icon">
+    <div>
+      <p>{{title}}</p>
+      <p v-html="desc"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Card",
+    props:{
+      icon:{
+        type:String,
+        default:"../../../assets/icon/member.svg"
+      },
+      title:String,
+      desc:String
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .ys-cart-item{
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    box-shadow:0 2px 10px 4px rgba(227,227,227,0.5);
+    border-radius:10px;
+
+    img{
+      width: 30px;
+      height: 30px;
+      object-fit:contain;
+    }
+
+    div{
+      padding: 8px 0;
+    }
+
+    p{
+      color: #000;
+      font-size: 5vw;
+      font-weight:500;
+      padding: 3px;
+    }
+
+    p:last-child{
+      color: #666666;
+      font-size: 3vw;
+    }
+  }
+</style>
