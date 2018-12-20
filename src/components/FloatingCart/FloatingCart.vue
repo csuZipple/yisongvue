25c81fe3f828ed919b54c395ec906745fe184355
csuZipple <csu_zipple@qq.com>
2018-12-20 11:47:39 +0800

    refactor: extract the float shopping cart component from the search(.vue) page

---
+++ src/components/FloatingCart/FloatingCart.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="bottom">
+    <div>
+      ￥1.00 <!--todo:compute current total-->
+    </div>
+    <div>
+      去结算
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FloatingCart"
+  }
+</script>
+
+<style lang="less" scoped>
+
+  .bottom{
+    height: 50px;
+    padding: 0 12px;
+    margin: 12px 0;
+
+    display: flex;
+    justify-content: space-between;
+
+    :first-child{
+      width: 70%;
+      background: rgba(51,51,51,0.6);
+      border-top-left-radius: 25px;
+      border-bottom-left-radius: 25px;
+      color: #ffffff;
+      justify-content: flex-end;
+      padding: 0 12px;
+
+      position: relative;
+      &:before{
+        content: '';
+        width: 80px;
+        height: 70px;
+        background: url("../../assets/icon/shopping-cart.svg") center no-repeat;
+        background-size: 100%;
+        position: absolute;
+        left: 5%;
+        top: -50%;
+      }
+    }
+    :last-child{
+      width: 30%;
+      background: #FFDF5C;
+      border-top-right-radius: 25px;
+      border-bottom-right-radius: 25px;
+    }
+    div{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 5vw;
+    }
+  }
+</style>
--- src/views/Search/Search.vue
+++ src/views/Search/Search.vue
@@ -17,14 +17,7 @@
       <div class="tips">
         <SearchItem v-for="(item,index) in searchTips" v-bind:key="index" v-bind="item" @add="updateCart" @sub="updateCart"/>
       </div>
-      <div class="bottom">
-        <div>
-          ￥1.00
-        </div>
-        <div>
-          去结算
-        </div>
-      </div>
+      <FloatingCart/>
     </div>
   </div>
 </template>
@@ -35,10 +28,11 @@
   import YsHeader from "../../components/Header";
   import {throttle} from "../../util/util";
   import SearchItem from "./components/SearchItem";
+  import FloatingCart from "../../components/FloatingCart/FloatingCart";
 
   export default {
     name: "Search",
-    components: {SearchItem, YsHeader, SearchTips},
+    components: {FloatingCart, SearchItem, YsHeader, SearchTips},
     methods:{
       onSearch(){
         console.log("search---",this.keyword);
@@ -49,7 +43,7 @@
         //todo:change this.searchTips to update search item
       },
       updateCart(item){
-        console.log(item);
+        //todo:update cart (add or sub) quantity.
       }
     },
     data(){
@@ -158,49 +152,6 @@
       .tips{
         overflow: scroll hidden;
         min-height: 400px; //prevent the soft keyboard from blocking
-      }
-
-      .bottom{
-        height: 50px;
-        padding: 0 12px;
-        margin: 12px 0;
-
-        display: flex;
-        justify-content: space-between;
-
-        :first-child{
-          width: 70%;
-          background: rgba(51,51,51,0.6);
-          border-top-left-radius: 25px;
-          border-bottom-left-radius: 25px;
-          color: #ffffff;
-          justify-content: flex-end;
-          padding: 0 12px;
-
-          position: relative;
-          &:before{
-            content: '';
-            width: 80px;
-            height: 70px;
-            background: url("../../assets/icon/shopping-cart.svg") center no-repeat;
-            background-size: 100%;
-            position: absolute;
-            left: 5%;
-            top: -50%;
-          }
-        }
-        :last-child{
-          width: 30%;
-          background: #FFDF5C;
-          border-top-right-radius: 25px;
-          border-bottom-right-radius: 25px;
-        }
-        div{
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          font-size: 5vw;
-        }
       }
     }
   }
