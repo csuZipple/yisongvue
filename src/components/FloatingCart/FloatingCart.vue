c7d001a6961156b46b45ab15ca28c5669426438b
csuZipple <csu_zipple@qq.com>
2018-12-21 09:25:33 +0800

    fix: update floating cart with new UI

--- src/components/FloatingCart/FloatingCart.vue
+++ src/components/FloatingCart/FloatingCart.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="bottom">
-    <div :class="{'active':isActive}" @click="onCartClicked">
-      ￥{{total}} <!--todo:compute current total-->
+    <div v-if="isActive">
+      <div class="active" @click="onCartClicked">
+        ￥{{total}} <!--todo:compute current total-->
+      </div>
+      <div @click="checkOut">
+        去结算
+      </div>
     </div>
-    <div @click="checkOut">
-      去结算
+    <div v-else>
+      <div style="font-size: 3vw; justify-content:center;">
+        未选购商品
+      </div>
+      <div style="background: rgba(51,51,51,0.6); font-size: 3vw;color: #ffffff;">
+        ￥1元起送
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -23,7 +34,7 @@
         this.$emit("checkout")
       },
       onCartClicked(){
-        this.$emit("cartClicked");
+        this.isActive()&&this.$emit("cartClicked");
       }
     },
     computed:{
@@ -44,45 +55,48 @@
   }
 
   .bottom{
-    height: 50px;
-    padding: 0 12px;
-    margin: 12px 0;
 
-    display: flex;
-    justify-content: space-between;
-
-    :first-child{
-      width: 70%;
-      background: rgba(51,51,51,0.6);
-      border-top-left-radius: 25px;
-      border-bottom-left-radius: 25px;
-      color: #ffffff;
-      justify-content: flex-end;
+    &>div{
+      height: 50px;
       padding: 0 12px;
+      margin: 12px 0;
+      display: flex;
+      justify-content: space-between;
 
-      position: relative;
-      &:before{
-        content: '';
-        width: 80px;
-        height: 70px;
-        background: url("../../assets/icon/shopping-cart-nothing.svg") center no-repeat;
-        background-size: 100%;
-        position: absolute;
-        left: 5%;
-        top: -50%;
+      :first-child{
+        width: 70%;
+        background: rgba(51,51,51,0.6);
+        border-top-left-radius: 25px;
+        border-bottom-left-radius: 25px;
+        color: #ffffff;
+        justify-content: flex-end;
+        padding: 0 12px;
+
+        position: relative;
+        &:before{
+          content: '';
+          width: 80px;
+          height: 70px;
+          background: url("../../assets/icon/shopping-cart-nothing.svg") center no-repeat;
+          background-size: 100%;
+          position: absolute;
+          left: 5%;
+          top: -50%;
+        }
+      }
+      :last-child{
+        width: 30%;
+        background: #FFDF5C;
+        border-top-right-radius: 25px;
+        border-bottom-right-radius: 25px;
+      }
+      div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 5vw;
       }
     }
-    :last-child{
-      width: 30%;
-      background: #FFDF5C;
-      border-top-right-radius: 25px;
-      border-bottom-right-radius: 25px;
-    }
-    div{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 5vw;
-    }
+
   }
 </style>
