cd9e62bfe7c4e426c43d925017e1e5d80e46921e
csuZipple <csu_zipple@qq.com>
2018-12-23 16:26:50 +0800

    feat: add keep-alive in nav pages

--- src/views/Container.vue
+++ src/views/Container.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="ys-index">
-    <router-view style="padding-bottom: 60px"/>
+    <keep-alive>
+      <router-view style="padding-bottom: 60px"/>
+    </keep-alive>
     <tabbar>
       <tabbar-item link="/nav/index" :selected="$route.path === '/nav/index'">
         <img slot="icon" src="../assets/icon/index.svg">
@@ -37,31 +39,31 @@
   import Tabbar from "../components/Tabbar/Tabbar";
   import TabbarItem from "../components/Tabbar/TabbarItem";
   //Check if the local cache needs to restore the previously crashed page
-    export default {
-        name: "Container",
-        components: {TabbarItem, Tabbar},
-        mounted(){
-          this.initToken();
-        },
-      methods:{
-          //if the locally cached token has not expired,use it to initialize the state
-          initToken(){
-              let initToken =getToken();
-              console.assert(initToken,"old token not found!");
-              /*for test --testing register
-              wxAuth()*/
-              if(initToken){
-                  console.log("init token with action");
-                  this.setToken(initToken);//=> this.$store.dispatch("setToken",initToken)
-              }
-          },
-        ...mapActions(["setToken"])
-      }
+  export default {
+    name: "Container",
+    components: {TabbarItem, Tabbar},
+    mounted(){
+      this.initToken();
+    },
+    methods:{
+      //if the locally cached token has not expired,use it to initialize the state
+      initToken(){
+        let initToken =getToken();
+        console.assert(initToken,"old token not found!");
+        /*for test --testing register
+        wxAuth()*/
+        if(initToken){
+          console.log("init token with action");
+          this.setToken(initToken);//=> this.$store.dispatch("setToken",initToken)
+        }
+      },
+      ...mapActions(["setToken"])
     }
+  }
 </script>
 
 <style scoped>
-.ys-index{
-  height: 100%;
-}
+  .ys-index{
+    height: 100%;
+  }
 </style>
