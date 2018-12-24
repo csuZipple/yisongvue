ff0379c20e4ffe88e0c7105aff2d97e1151de533
csuZipple <csu_zipple@qq.com>
2018-12-24 14:51:08 +0800

    fix: change redirect url to login page

--- src/util/wx/config/constant.js
+++ src/util/wx/config/constant.js
@@ -4,7 +4,7 @@ const wx = {
    appID:  "wx542b343e4c584351",
    scope:"snsapi_userinfo",
 };
-const AUTH =`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wx.appID}&redirect_uri=${httpConfig.register}&response_type=code&scope=${wx.scope}&state=STATE#wechat_redirect`;
+const AUTH =`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wx.appID}&redirect_uri=${httpConfig.login}&response_type=code&scope=${wx.scope}&state=STATE#wechat_redirect`;
 export {
   AUTH
 };
