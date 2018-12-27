cd10342a057b182faf12efb09ef5432360c75ff1
csuZipple <csu_zipple@qq.com>
2018-12-27 16:43:01 +0800

    fix: change appID

--- src/util/wx/config/constant.js
+++ src/util/wx/config/constant.js
@@ -1,7 +1,7 @@
 import httpConfig from '../../http/constant'
 
 const wx = {
-   appID:  "wx542b343e4c584351",
+   appID:  "wxe80557e8ca49c4b9",
    scope:"snsapi_userinfo",
 };
 const AUTH =`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wx.appID}&redirect_uri=${httpConfig.login}&response_type=code&scope=${wx.scope}&state=STATE#wechat_redirect`;
