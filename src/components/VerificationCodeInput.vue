99210c265a2d6d0675d2759bd1caa7fd92f1a7fa
csuZipple <csu_zipple@qq.com>
2018-12-28 19:07:09 +0800

    feat: finish writing verification code component

--- src/components/VerificationCodeInput.vue
+++ src/components/VerificationCodeInput.vue
@@ -1,13 +1,86 @@
 <template>
-    $END$
+  <div class="ys-verification">
+    <div class="input-wrapper" v-for="item in amount" :key="item">
+      <input type="text" title="code" v-focus="(item - 1) === currentIndex" maxlength="1" @input="handleInput($event,(item-1))" v-model="code[item-1]">
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "VerificationCodeInput"
+  export default {
+    name: "VerificationCodeInput",
+    props:{
+      amount:{
+        type:Number,
+        default:4
+      }
+    },
+    directives: {
+      focus: {
+        inserted: function (el,obj) {
+          obj.value && el.focus();
+        },
+        componentUpdated: function(el,obj) {
+          obj.value && el.focus();
+        }
+      }
+    },
+    created(){
+      this.code = new Array(this.amount).fill("");
+    },
+    methods:{
+      handleInput(e,index){
+        this.currentIndex = index;
+        e.target.value = this.validateNumber(e.target.value);
+        e.target.value!==''&& ++this.currentIndex;
+        this.currentIndex === this.amount && this.$emit("onCompleted",this.code.join(""))
+      },
+      validateNumber(val){
+        return val.replace(/\D/g,'');
+      },
+    },
+    data(){
+      return{
+        code:[],
+        currentIndex:0
+      }
+    },
+    watch:{
+      code(code){
+        console.log("change!",code);
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .ys-verification{
+    width:100%;
+    display: flex;
+    justify-content: space-around;
 
+    .input-wrapper{
+      border-bottom: 1px solid #D6D6D6;
+      width: 20%;
+      height: 0;
+      padding-bottom:20%;
+      position: relative;
+
+      input{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: all 0.3s;
+        font-size: 7vw;
+        color: #333333;
+      }
+
+    }
+
+
+    input:focus{
+      border-bottom: 1px solid #666666;
+    }
+  }
 </style>
